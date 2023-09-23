b8cb79b2b8d08bef71f2bf8e9d4b38ae85bb6103
Isadora Rodrigues <isadoradorarodrigues@gmail.com>
2023-09-23 11:55:55 -0300

    Update debugging_exercises.rb

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -6,11 +6,11 @@
 # To the top, the first line, where the error occured, and ONLY THEN fix the error
 
 def decrement_smallest_value(nested_array)
-  smallest_value = nested_array.flatten.max
+  smallest_value = nested_array.flatten.min
   nested_array.each do |array|
     array.each do |current_value|
       if smallest_value > current_value
-        smallest_value = current_valu
+        smallest_value = current_value
       end
     end
   end
@@ -21,11 +21,11 @@ end
 # Don't look at this method prior to running the test
 # Run rspec, let the test fail, and go through the stack trace again
 def increment_greatest_value(nested_array)
-  greatest_value = nested_array.flatten.min
+  greatest_value = nested_array.flatten.max
   nested_array.each do |array|
     array.each do |current_value|
-      if greatest_value < nil
-        greatest_value = current_value
+      if current_value == nil 
+        greatest_value > nil
       end
     end
   end
@@ -39,7 +39,7 @@ def isogram?(string)
   original_length = string.length
   string_array = string.downcase.split
   unique_length = string_array.uniq.length
-  original_length == unique_length
+  original_length != unique_length
 end
 
 # Can you guess what's next?
@@ -51,6 +51,6 @@ end
 
 def yell_greeting(string)
   name = string
-  name = name.downcase
+  name = string.upcase
   greeting = "WASSAP, #{name}!"
 end
